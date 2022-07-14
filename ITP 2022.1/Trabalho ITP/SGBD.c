c8e1c285af8a90203125dd15570fead050b4c228
fabris2023 <109186939+fabris2023@users.noreply.github.com>
2022-07-13 22:01:47 -0300

    Update SGBD.c

--- ITP 2022.1/Trabalho ITP/SGBD.c
+++ ITP 2022.1/Trabalho ITP/SGBD.c
@@ -38,26 +38,94 @@ typedef struct
 } PedidoItens;
 
 
-
+void cadastraProduto(Produto *produto, int *indiceProdut){
+  if(*indiceProdut == 0){
+    produto = calloc(1, sizeof(Produto));
+  }else{
+    printf("%d", *indiceProdut);
+    produto = realloc(produto, sizeof(Produto)*10);
+  }
+  printf("Digite um código para o produto:\n");
+  scanf("%d",&produto[*indiceProdut].idProduto);
+  printf("Digite o nome do produto:\n");
+  scanf("%s",produto[*indiceProdut].descricao);
+  printf("Digite o preço do produto:\n");
+  scanf("%f",&produto[*indiceProdut].preco);
+  *indiceProdut = *indiceProdut + 1;
+}
 
 
 int main (void) {
 
-printf("");
+  int opcao = 0, indiceProd;
 
-printf(" ================================================\n");
-printf("|       = SEJA BEM VINDO AO SISTEMA CRUD =       |\n");
-printf(" ================================================\n");
-printf("| ESCOLHA UMA OPCAO:                             |\n");
-printf("| 1 - insere um produto no cadastro              |\n");
-printf("| 2 - lista/altera/remove produto(s) do cadastro |\n");
-printf("| 3 - insere um cliente no cadastro              |\n");
-printf("| 4 - lista/altera/remove cliente(s) do cadastro |\n");
-printf("| 5 - insere um novo pedido                      |\n");
-printf("| 6 - lista os pedidos ja realizados             |\n");
-printf("| 7 - exibe nota fiscal de um pedido             |\n");
-printf("| 9 - encerra o programa                         |\n");
-printf(" ================================================");
+  indiceProd = 0;
+  
+  Produto *produtos;
+  Cliente *clientes;
 
-return 0;
+
+  while(opcao!=9){
+    
+    printf("");
+
+    printf("       ================================================\n");
+    printf("|       = SEJA BEM VINDO AO SISTEMA CRUD =     |\n");
+    printf(" ================================================\n");
+    printf("| ESCOLHA UMA OPCAO:                             |\n");
+    printf("| 1 - insere um produto no cadastro              |\n");
+    printf("| 2 - lista/altera/remove produto(s) do cadastro |\n");
+    printf("| 3 - insere um cliente no cadastro              |\n");
+    printf("| 4 - lista/altera/remove cliente(s) do cadastro |\n");
+    printf("| 5 - insere um novo pedido                      |\n");
+    printf("| 6 - lista os pedidos ja realizados             |\n");
+    printf("| 7 - exibe nota fiscal de um pedido             |\n");
+    printf("| 9 - encerra o programa                         |\n");
+    printf("       ================================================\n");
+    scanf("%d", &opcao);
+    if(opcao==1){
+      int opcaoInterna;
+      cadastraProduto(produtos,&indiceProd);
+      while(opcaoInterna!=0){
+        printf("Deseja cadastrar mais algum produto(1 para sim/ 0 para não)?");
+        
+        scanf("%d", &opcaoInterna);
+
+        if(opcaoInterna==1){
+          cadastraProduto(produtos,&indiceProd);
+        }else if(opcaoInterna!=0){
+          
+          while(opcaoInterna!=0||opcaoInterna!=1){
+            printf("Opção inválida");
+            scanf("%d", &opcaoInterna);
+          }
+          if(opcaoInterna==1){
+            cadastraProduto(produtos,&indiceProd);
+          }
+          
+        }
+      }
+      
+    }else if(opcao == 2){
+      
+    }else if(opcao == 3){
+      
+    }else if(opcao == 4){
+      
+    }else if(opcao == 5){
+      
+    }else if(opcao == 6){
+      
+    }else if(opcao == 7){
+      
+    }else if(opcao == 8){
+      
+    }else if(opcao == 9){
+      
+    }else{
+      
+    }  
+  }
+
+  return 0;
 }
